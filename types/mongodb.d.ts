852220c5964803d54a04dadbf398595b502c6731
Xavi Barnes <xbarnesortega@gmail.com>
2026-01-10 08:20:01 +0100

    fix: Add Db and Collection types to mongodb declarations

--- types/mongodb.d.ts
+++ types/mongodb.d.ts
@@ -4,10 +4,29 @@ declare module 'mongodb' {
     toString(): string
   }
   
+  export interface Db {
+    collection<T = any>(name: string): Collection<T>
+    command(command: any): Promise<any>
+  }
+  
+  export interface Collection<T = any> {
+    find(query: any): Cursor<T>
+    findOne(query: any): Promise<T | null>
+    insertOne(doc: T): Promise<{ insertedId: ObjectId }>
+    updateOne(filter: any, update: any, options?: any): Promise<any>
+    deleteOne(filter: any): Promise<{ deletedCount: number }>
+    findOneAndUpdate(filter: any, update: any, options?: any): Promise<{ value: T | null }>
+  }
+  
+  export interface Cursor<T = any> {
+    toArray(): Promise<T[]>
+    sort(sort: any): Cursor<T>
+  }
+  
   export class MongoClient {
     constructor(url: string, options?: any)
     connect(): Promise<MongoClient>
-    db(name?: string): any
+    db(name?: string): Db
     close(): Promise<void>
   }
   
